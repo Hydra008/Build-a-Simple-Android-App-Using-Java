75fc1a8f52e5463c1675885506fdfe7978ffcbe8
Hydra008 <kshah2@lakeheadu.ca>
2017-09-15 14:39:42 -0300

    Create FunFactsActivity.java

---
+++ Challenges/initialize a button/FunFactsActivity.java
@@ -0,0 +1,19 @@
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class FunFactsActivity extends Activity {
+  
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fun_facts);
+
+        // Declare our View variables and assign them the Views from the layout file
+        TextView factLabel = (TextView) findViewById(R.id.factTextView);
+        Button showFactButton = (Button) finfViewById(R.id.showFactButton);
+    }
+}
+
